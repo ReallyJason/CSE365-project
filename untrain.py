515bbe0bc1eb9850a62256e7f553f54322eaa8ee
Jason Hu <jasonhu@Jasons-MacBook-Pro-5.local>
2025-12-09 15:35:08 -0500

    fixed untrained

--- untrain.py
+++ untrain.py
@@ -2,43 +2,28 @@ import torch
 import numpy as np
 from train import GenreClassifier
 
-
-# ---------------------------------------------------
-# Load dataset
-# ---------------------------------------------------
 data = np.load("preprocessed_data.npz")
 
 X_test = torch.tensor(data["X_test"], dtype=torch.float32)
 mapping = list(data["mapping"])
 
 X_test = X_test.permute(0, 3, 1, 2)
-
-samples = X_test[:5]  # first 5 samples
-
+samples = X_test[:5]
 C, H, W = samples.shape[1:]
 NUM_CLASSES = len(data["mapping"])
 
-
-# ---------------------------------------------------
-# Create untrained model
-# ---------------------------------------------------
 model = GenreClassifier(C, H, W, NUM_CLASSES)
 model.eval()
-
 
 def softmax(x):
     e = np.exp(x - np.max(x))
     return e / e.sum()
 
-
-# To store means
 sample_means = []
-all_probabilities = []
 
-
-print("\n===================================")
-print("   UNTRAINED MODEL — MEAN ONLY")
-print("===================================\n")
+print("\n=====================")
+print("   UNTRAINED MODEL   ")
+print("=====================\n")
 
 for i in range(len(samples)):
     x = samples[i].unsqueeze(0)
